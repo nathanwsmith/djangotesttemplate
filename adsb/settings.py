36c7d3757a4438a7f8d6b865810386f815d0e9d5
Lasciella <d.c.wood@outlook.com>
2020-11-12 15:43:42 +0000

    Changed allowed hosts

--- adsb/settings.py
+++ adsb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@=k17h*y0%cuodmez0gzirpkjs$jda(w(o*cy1-l84!6-!(_n7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
